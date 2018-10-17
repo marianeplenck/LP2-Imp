7a727751e5fc9cf2d12b0be4611e1a746e058ce4
Mariane Plenckauskas <42036313+marianeplenck@users.noreply.github.com>
2018-10-17 11:16:06 -0300

    Add files via upload

--- revisao_lp2_dicionario.py
+++ revisao_lp2_dicionario.py
@@ -3,12 +3,14 @@ e os valores de cada chave são o número de ocorrências de caractere na string
 
 #Exemplo caracteres('Banana') -> {'B':1, 'a':3, 'n':2}
 
-def caracteres(s):
+'''def caracteres(s): primeira versão
     d={}
     #for i in range(2121213)
     #for item in alguma coisa
     for c in s:
         d[c] = 1 #quase funciona
+'''
+
 
 '''<problema> <se a chave não existe o programa cria e coloca 1, se a    
 chave existe o programa não aumenta e sim cria um novo também;>'''
@@ -27,8 +29,31 @@ def caractere(s):#lógica para str
 
     return d
 
+def test_caractere_1(): #teste que dará certo
+    assert caractere('ana') == {'a':2, 'n':1}
+    return True #se quiser ver a resposta,se retornar TRUE deu certo caso contrário dá um erro
 
-#desenvolva a lógica para percorrer um int ( com while) 
+def test_caractere_2(): #teste que dará errado
+    assert caractere('ana') == {'a':777, 'n':1}
+    return True #se quiser ver a resposta,se retornar TRUE deu certo caso contrário dá um erro
+
+# desenvolva a lógica para percorrer um int ( com while) 
 # estude percorrer um dic. append, etc 
-# vai cair uma questão semelhante a esta na prova        
+# vai cair uma questão semelhante a esta na prova 
+
+def caracteres(n):#outra versão
+    for c in n:
+        try:
+            d[c]= d[c] + 1
+        except KeyError:
+            d[c] = 1
+    return d
+
+
+#-------------------------------------------------------------------------------------------------------------------------------
+def mostra(lista, i, f):
+    #qual o erro que pode acontecer se i>=f
+    for i in  range (i, f+1):
+        print(lista[j]) # existe problema lógico pq se o inicio começa depois do final não existe lista
+        '''-->IndexError'''
 
